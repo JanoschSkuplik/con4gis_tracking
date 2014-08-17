df499ef9d8f5fc47b03bc29550f541d3db0578de
JanoschSkuplik <kontakt@janosch-skuplik.de>
2014-08-17 14:15:41 +0200

    Contao-Model integriert.

---
+++ con4gis_tracking/models/C4gTrackingPositionsModel.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @version   php 5
+ * @package   con4gis_tracking
+ * @author    Janosch Oltmanns
+ * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright Janosch Oltmanns in cooperation with Küstenschmiede GmbH Software & Design 2014
+ * @link      http://janosch-oltmanns.de https://www.kuestenschmiede.de
+ */
+
+
+/**
+ * Run in a custom namespace, so the class can be replaced
+ */
+namespace JanoschOltmanns;
+
+
+/**
+ * Reads and writes tracking positions
+ *
+ * @package   con4gis_tracking
+ * @author    Janosch Oltmanns
+ * @copyright Janosch Oltmanns in cooperation with Küstenschmiede GmbH Software & Design 2014
+ */
+class C4gTrackingPositionsModel extends \Model
+{
+
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_c4g_tracking_positions';
+
+}
+
+
+
