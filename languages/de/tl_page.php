49aa2efcedd69d604396dea4c0f2fc469886dbc6
JanoschSkuplik <kontakt@janosch-skuplik.de>
2015-06-19 08:41:15 +0200

    Benennung der Tracking-Konfiguration in der Seitenstruktur.

---
+++ languages/de/tl_page.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @version   php 5
+ * @package   con4gis_tracking
+ * @author    Janosch Oltmanns
+ * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright Janosch Oltmanns in cooperation with Küstenschmiede GmbH Software & Design 2014 - 2015
+ * @link      http://janosch-oltmanns.de https://www.kuestenschmiede.de
+ */
+
+$GLOBALS['TL_LANG']['tl_page']['c4gtracking_legend'] = "Tracking-Konfiguration";
+
+$GLOBALS['TL_LANG']['tl_page']['c4gtracking_configuration'] = array('Tracking-Konfiguration', 'Hier kann diesem Seitenbaum eine Tracking-Konfiguration zugewiesen werden.');
+
